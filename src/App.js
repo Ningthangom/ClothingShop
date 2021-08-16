a45926556a2de4f7f9cf5ee1cd8e17169863f602
Ningthangom <ningthangom@gmail.com>
2021-08-16 19:29:26 +0930

    worked on Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
-
 
 import HomePage from './pages/homepages/homepage.component'
 
+
+const HatsPage = () => (
+<div>
+  <h1>Hats pages</h1>
+</div>
+)
+
 function App() {
   return (
-   
-    <HomePage/>
+    <div>
+      <Switch>
+      <Route exact path='/' component={HomePage}/>
+    <Route  path='/hats' component={HatsPage}/>
+    </Switch>
+    </div>
+    
   );
 }
 
--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -1,10 +1,14 @@
 
 import React from 'react';
+// withRouter is a higher order component 
+import {withRouter} from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
+const MenuItem = ({title, imageUrl, size,history, match, linkUrl}) => (
 
-    <div style={{ backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
+    <div style={{ backgroundImage: `url(${imageUrl})`}} 
+            className={`${size} menu-item`}
+            onClick={() =>history.push(`${match.url}${linkUrl}`)}>
             <div
                 className='background-image'
                 style={{
@@ -18,4 +22,6 @@ const MenuItem = ({title, imageUrl, size}) => (
             </div> 
 )
 
-export default MenuItem;
+// if menuitem is exported with withRouter then it will now give  me access 
+// to those location match and history props
+export default withRouter(MenuItem);
