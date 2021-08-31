4b04db998631c02d8940136bee2e14ccfa889114
Ningthangom <ningthangom@gmail.com>
2021-08-31 17:41:28 +0930

    added dropdwon for cart with its reducer

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -9,12 +9,13 @@ import {connect } from 'react-redux';
 import {ReactComponent as Logo } from '../../assets/crown.svg'
 import {auth} from '../../firebase/firebase.util'
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss'
 
 
 // this header component will be functional component 
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden }) => (
   
     <div className="header">   
     <Link to ="/" className="logo-container">
@@ -32,15 +33,20 @@ const Header = ({currentUser}) => (
           SIGN IN
         </Link>
       )}
+      
       <CartIcon/>
     </div>
+    {hidden ? null : 
+      <CartDropDown/>}
+    
     </div>
 )
 
 // this state is top level root reducer
-const mapStateToProps =state =>( {
+const mapStateToProps =({user: {currentUser}, cart: {hidden}}) =>( {
 
     // s     rootreducer=>user value(this will give us userreducer) => currentUser 
-  currentUser:  state.user.currentUser
+  currentUser,
+  hidden
 });
 export default connect(mapStateToProps)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,6 +5,7 @@ import {combineReducers} from 'redux'
 
 
 import userReducer from './user/user.reducer'
+import cartReducer from './cart/cart.reducers'
 
 // full state in redux is one big json object 
 // the key that represent individual states, ie actual reducers, is the actual reducers 
@@ -12,4 +13,5 @@ import userReducer from './user/user.reducer'
 export default combineReducers({
     // key 'user' goes to userReducer
     user:userReducer,
+    cart:cartReducer,
 })
