465af8d572cf5b9fe5cc27e0516e72b6a38e3025
Ningthangom <ningthangom@gmail.com>
2021-08-31 11:02:39 +0930

    add cart icon

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -8,6 +8,7 @@ import {connect } from 'react-redux';
 // how to import svg 
 import {ReactComponent as Logo } from '../../assets/crown.svg'
 import {auth} from '../../firebase/firebase.util'
+import CartIcon from '../cart-icon/cart-icon.component';
 import './header.styles.scss'
 
 
@@ -31,6 +32,7 @@ const Header = ({currentUser}) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon/>
     </div>
     </div>
 )
